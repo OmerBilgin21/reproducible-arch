dfc5967002aa9edb02a776122624834f22d409de
oemer-lendis <oemer.bilgin@lendis.de>
2026-02-15 18:55:19 +0100

    feat: do not execute queries on save and define global save location

--- config/nvim/lua/plugins/dadbod.lua
+++ config/nvim/lua/plugins/dadbod.lua
@@ -4,7 +4,7 @@ return {
     {
       "tpope/vim-dadbod",
       lazy = true,
-      { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true }
+      { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
     },
   },
   cmd = {
@@ -14,6 +14,8 @@ return {
     "DBUIFindBuffer",
   },
   init = function()
+    vim.g.db_ui_execute_on_save = 0
+    vim.g.db_ui_save_location = vim.fn.expand("~/queries")
     vim.g.db_ui_use_nerd_fonts = 1
   end,
 }
