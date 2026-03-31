3da2b21d9bb86a509d1bb95da49fcdb1133b9513
OmerBilgin21 <oemerbilgin@proton.me>
2026-03-31 22:54:54 +0200

    feat: add godoc

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -1,5 +1,50 @@
 return {
   {
+    "fredrikaverpil/godoc.nvim",
+    version = "*",
+    dependencies = {
+      { "nvim-telescope/telescope.nvim" }, -- optional
+      {
+        "nvim-treesitter/nvim-treesitter",
+        branch = "main",
+        build = ":TSUpdate godoc go", -- install/update parsers
+        config = function()
+          require("nvim-treesitter.parsers").godoc = {
+            install_info = {
+              url = "https://github.com/fredrikaverpil/tree-sitter-godoc",
+              files = { "src/parser.c" },
+              version = "*",
+            },
+            filetype = "godoc",
+          }
+
+          -- Map godoc filetype to use godoc parser
+          vim.treesitter.language.register("godoc", "godoc")
+
+          -- Enable :TSInstall godoc, :TSUpdate godoc
+          vim.api.nvim_create_autocmd("User", {
+            pattern = "TSUpdate",
+            callback = function()
+              require("nvim-treesitter.parsers").godoc = parser_config
+            end,
+          })
+
+          -- Enable godoc filetype for .godoc files (optional)
+          vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+            pattern = "*.godoc",
+            callback = function()
+              vim.bo.filetype = "godoc"
+            end,
+          })
+        end,
+      },
+    },
+    build = "go install github.com/lotusirous/gostdsym/stdsym@latest", -- optional
+    cmd = { "GoDoc" }, -- optional
+    ft = "godoc", -- optional
+    opts = { picker = { type = "telescope" } }, -- see further down below for configuration
+  },
+  {
     "folke/lazydev.nvim",
     ft = "lua",
     opts = {
