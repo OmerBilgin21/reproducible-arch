5de48c96a12101501a3ee55d2b9c34ca0deb39c1
OmerBilgin21 <oemerbilgin@proton.me>
2026-04-05 09:03:08 +0200

    feat: improve debugger ux

--- config-shared/nvim/lua/plugins/debugger.lua
+++ config-shared/nvim/lua/plugins/debugger.lua
@@ -5,14 +5,6 @@ return {
     dependencies = { "mfussenegger/nvim-dap" },
     config = function()
       require("dap-go").setup({
-        dap_configurations = {
-          {
-            type = "go",
-            name = "Attach remote",
-            mode = "remote",
-            request = "attach",
-          },
-        },
         delve = {
           path = vim.fn.stdpath("data") .. "/mason/bin/dlv",
         },
@@ -22,12 +14,57 @@ return {
   {
     "rcarriga/nvim-dap-ui",
     dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+    keys = {
+      -- debug the test function your cursor is inside
+      {
+        "<leader>dt",
+        function()
+          require("dap-go").debug_test()
+        end,
+        desc = "Debug test",
+      },
+      -- breakpoints
+      {
+        "<leader>db",
+        function()
+          require("dap").toggle_breakpoint()
+        end,
+        desc = "Toggle breakpoint",
+      },
+      -- execution control
+      {
+        "<F5>",
+        function()
+          require("dap").continue()
+        end,
+        desc = "Continue",
+      },
+      {
+        "<F10>",
+        function()
+          require("dap").step_over()
+        end,
+        desc = "Step over",
+      },
+      {
+        "<F11>",
+        function()
+          require("dap").step_into()
+        end,
+        desc = "Step into",
+      },
+      {
+        "<F12>",
+        function()
+          require("dap").step_out()
+        end,
+        desc = "Step out",
+      },
+    },
     config = function()
       local dap, dapui = require("dap"), require("dapui")
       dapui.setup()
-      dap.listeners.before.attach.dapui_config = function()
-        dapui.open()
-      end
+      -- open/close the UI automatically when a debug session starts/ends
       dap.listeners.before.launch.dapui_config = function()
         dapui.open()
       end
@@ -40,23 +77,14 @@ return {
     end,
   },
   {
-
     "OmerBilgin21/print-debugger.nvim",
-    -- name = "print-debugger",
     -- dir = "/home/oemer/projects/print-debugger",
     version = false,
     config = function()
       require("print-debugger").setup({
-        typescript = {
-          prefix = "logger.info",
-        },
-        go = {
-          prefix = "internal.Logger",
-          spread_mode = true,
-        },
-        keymaps = {
-          "<C-g>",
-        },
+        typescript = { prefix = "logger.info" },
+        go = { prefix = "internal.Logger", spread_mode = true },
+        keymaps = { "<C-g>" },
       })
     end,
   },
