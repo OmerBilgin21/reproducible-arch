b99a9a5df6c2e623ebc34b7b8391a66c7c088375
OmerBilgin21 <oemerbilgin@proton.me>
2026-06-23 23:22:46 +0200

    feat: adopt to hyprland changes for restowing

--- re-stow.sh
+++ re-stow.sh
@@ -4,6 +4,13 @@ set -euo pipefail
 
 OS="$(uname -s)"
 
+# because we do the --adopt then discard changes trick for hyprland.conf
+# this exit is good to have, I don't wanna lose my changes if I have some
+git diff --quiet HEAD ./config-linux/hypr/hyprland.conf || {
+  echo "You have changes in hyprland.conf, those would be lost. First commit them before running re-stow.sh"
+  exit 1
+}
+
 stow -D -t ~/.config config-shared
 stow -D -t ~/.local/bin localbin-shared
 stow -D -t ~/.emacs.d emacs
@@ -20,10 +27,16 @@ if [[ "$OS" == "Linux" ]]; then
   stow -D -t ~/ home-linux
   stow -D -t ~/.config config-linux
   stow -D -t ~/.local/bin localbin-linux
-  stow -t ~/.config config-linux
+  # I have to use --adopt here because apparently hyprland changed behaviour
+  # and is now always spitting out a hyprland.conf file if it's absent.
+  stow -t ~/.config config-linux --adopt
+  git checkout -- ./config-linux/hypr/hyprland.conf
   stow -t ~/.local/bin localbin-linux
   stow -t ~/ home-linux
-  hyprctl reload
+  sleep 3
+  hyprctl reload # for waybar
+  sleep 1
+  hyprctl reload # for hyprland
   restart-app waybar
 elif [[ "$OS" == "Darwin" ]]; then
   stow -D -t ~/.config config-mac
