875276c6955dd715d37e602c32449675256d7bea
OmerBilgin21 <oemerbilgin@proton.me>
2026-06-23 23:22:07 +0200

    fix: properly configure octo

--- config-shared/nvim/lua/plugins/diff.lua
+++ config-shared/nvim/lua/plugins/diff.lua
@@ -4,5 +4,32 @@ return {
   },
   {
     "pwntester/octo.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+      "nvim-tree/nvim-web-devicons",
+    },
+    cmd = "Octo",
+    opts = {
+      picker = "telescope",
+      enable_builtin = true,
+    },
+    keys = {
+      {
+        "<leader>op",
+        "<CMD>Octo pr list<CR>",
+        desc = "List GitHub PullRequests",
+      },
+      {
+        "<leader>or",
+        "<CMD>Octo review<CR>",
+        desc = "Review GitHub PullRequests",
+      },
+      {
+        "<leader>os",
+        "<CMD>Octo review submit<CR>",
+        desc = "Submit PullRequests review",
+      },
+    },
   },
 }
